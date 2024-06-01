c79b0a280d578b0f7c79e1a21c0379bc2e979361
Crafty12345 <hunterdylan482@gmail.com>
2024-06-01 11:22:41 +0800

    Quartz sync: Jun 1, 2024, 11:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Chasing Hearts Wiki",
+    pageTitle: "Chasing❤️Hearts Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
